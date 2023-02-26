bd1539e18b3a0cba023f3c5cc65df1e9d5466684
c29m <c29.hunter@gmail.com>
2023-02-26 00:29:18 -0300

    fanc01 - aula 2 - Recebendo os dados de um filme

---
+++ fanc01-criando-uma-web-api/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdcionaFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+
+        Console.WriteLine(filme.Titulo);
+        Console.WriteLine(filme.Genero);
+        Console.WriteLine(filme.duracao);
+    }
+}
